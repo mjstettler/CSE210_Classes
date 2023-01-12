eb41fb126c86de191705c76f7714fb0f779cd0a7
Mikey Stettler <mjstettler@gmail.com>
2023-01-12 10:16:38 -0700

    Completed the first 2 steps of the instructions

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magic = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("What is your guess? ");
+        int guess = Convert.ToInt32(Console.ReadLine());
+        do
+        {
+            if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+                guess = Convert.ToInt32(Console.ReadLine());
+            }
+
+            else if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("What is your guess? ");
+                guess = Convert.ToInt32(Console.ReadLine());
+            }
+        } while (guess != magic);
+
+        Console.WriteLine("You guessed it! Great job!");
     }
 }
