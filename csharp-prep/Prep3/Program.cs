d9767630c57174d8b8eb28f4e512602b7e41a874
Mikey Stettler <mjstettler@gmail.com>
2023-01-12 10:39:54 -0700

    Completed step 3 and both stretch activities

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,28 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        int magic = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("What is your guess? ");
-        int guess = Convert.ToInt32(Console.ReadLine());
-        do
+        int attempts = 0;
+        string response = "yes";
+        while (response == "yes")
         {
-            if (guess < magic)
-            {
-                Console.WriteLine("Higher");
-                Console.Write("What is your guess? ");
-                guess = Convert.ToInt32(Console.ReadLine());
-            }
+            Console.WriteLine("Can you guess the magic number? ");
+            Random randomGenerator = new Random();
+            int magic = randomGenerator.Next(1, 11);
 
-            else if (guess > magic)
+            Console.Write("What is your guess? ");
+            attempts += 1;
+            int guess = Convert.ToInt32(Console.ReadLine());
+            do
             {
-                Console.WriteLine("Lower");
-                Console.Write("What is your guess? ");
-                guess = Convert.ToInt32(Console.ReadLine());
-            }
-        } while (guess != magic);
+                if (guess < magic)
+                {
+                    Console.WriteLine("Higher");
+                    Console.Write("What is your guess? ");
+                    attempts += 1;
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
 
-        Console.WriteLine("You guessed it! Great job!");
+                else if (guess > magic)
+                {
+                    Console.WriteLine("Lower");
+                    Console.Write("What is your guess? ");
+                    attempts += 1;
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+            } while (guess != magic);
+
+            Console.WriteLine($"You guessed it in {attempts}! Great job!");
+            attempts = 0;
+            Console.WriteLine("Do you want to play again? (yes or no) ");
+            response = Console.ReadLine();
+        }
+        Console.WriteLine("Okay thanks for playing!");
     }
 }
