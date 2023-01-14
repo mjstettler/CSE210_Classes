cb62fb1e751ec59797e32cf83df5c517450c2490
Mikey Stettler <mjstettler@gmail.com>
2023-01-14 07:48:03 -0700

    Created the base structure for adding number to a list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of Numbers, type 0 when finished.");
+        
+        List<int> numbers = new List<int>();
+
+        int input = 1;
+        while (input != 0)
+        {
+            Console.Write("Enter a number:  ");
+            input = int.Parse(Console.ReadLine());
+            if (input == 0)
+            {
+                input = 0;
+            }
+            else
+            {
+                numbers.Add(input);
+            }
+            
+        }
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
