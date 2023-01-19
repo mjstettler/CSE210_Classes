aae0a644e765bab6cb6216012e1e62d23c2000d7
Mikey Stettler <mjstettler@gmail.com>
2023-01-18 18:11:11 -0700

    struggled to understand how to do this lesson at all! Instructions not clear.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Garmin";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Developer";
+        job2._company = "Github";
+        job2._startYear = 1991;
+        job2._endYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._name = "Mike Stettler";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
     }
 }
