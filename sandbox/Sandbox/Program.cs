d6a44b9bbeb248d485b0b403dd2133f9957b1efa
Mikey Stettler <mjstettler@gmail.com>
2023-01-28 23:32:40 -0700

    Testing AddRange

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Collection of string
+        string[] animals = { "Cow", "Camel", "Elephant", "Horse" };
+        // Create a List and add a collection
+        List<string> animalsList = new List<string>();
+        animalsList.AddRange(animals);
+        foreach (string a in animalsList)
+        Console.WriteLine(a);
+        int numberInlist = animalsList.Count;
+        Console.WriteLine(numberInlist);
+        
     }
 }
