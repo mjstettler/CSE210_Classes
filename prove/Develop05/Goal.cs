d9a496378da735df4c9010ba9ce4d95b03823aa3
Mikey Stettler <mjstettler@gmail.com>
2023-03-10 16:39:58 -0700

    Started adding methods to the Goal class

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -7,6 +7,40 @@ abstract public class Goal
     int _pointValue;
     bool _isCompleted = false;
 
+    public Goal(string aGoalName, string aGoalDescription)
+    {
+        _goalName = aGoalName;
+        _goalDescription = aGoalDescription;
+
+    }
+
+    public void SetGoalName(string aGoalName)
+    {
+        _goalName = aGoalName;
+    }
+
+    public string GetGoalName()
+    {
+        return _goalName;
+    }
+    public void SetGoalDescription(string aGoalDescription)
+    {
+        _goalDescription = aGoalDescription;
+    }
+    public string GetGoalDescription()
+    {
+        return _goalDescription;
+    }
+
+    public string GetGoalStatus()
+    {
+        if (_isCompleted)
+        {
+            return "x";
+        }
+    return " ";
+    }
+
     abstract public int SetPoints();
 
 }
